33a5247fdf6c4fcbbb1b186a000035a798ddf8ef
Anuradha <anudate97@gmail.com>
2020-04-01 01:41:17 +0530

    final

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,7 +9,12 @@ import {
   FileTransfer,
   FileTransferObject
 } from "@ionic-native/file-transfer/ngx";
-
+import { HttpClientModule } from "@angular/common/http";
+//import { HttpModule } from "@angular/http";
+declare var require: any;
+const FileSaver = require("file-saver");
+import { FileOpener } from "@ionic-native/file-opener/ngx";
+//import { FileChooser } from "@ionic-native/file-chooser/ngx";
 @Component({
   selector: "app-home",
   templateUrl: "home.page.html",
@@ -17,14 +22,15 @@ import {
 })
 export class HomePage {
   ft: any;
-  fileOpener: any;
+  // fileOpener: any;
   fileTransfer: FileTransferObject;
   constructor(
     private platform: Platform,
     private file: File,
     private transfer: FileTransfer,
     private document: DocumentViewer,
-    private alertController: AlertController
+    private alertController: AlertController,
+    private fileOpener: FileOpener //private fileChooser: FileChooser
   ) {
     this.fileTransfer = this.transfer.create();
   }
@@ -33,38 +39,50 @@ export class HomePage {
     const opt: DocumentViewerOptions = {
       title: "My PDF"
     };
-    this.document.viewDocument("assets/MDB.pdf", "application/pdf", opt);
+    FileSaver.saveAs(
+      "https://devdactic.com/html/5-simple-hacks-LBT.pdf",
+      "myFile.pdf"
+    );
   }
 
   downloadAndOpenLocalPdf() {
-    this.presentAlert('error');
-    let path = null;
+    /*  let path = null;
     path = this.file.dataDirectory;
+    // this.presentAlert("CHECK" + this.file);
 
+    //this.presentAlert("CHECK" + this.fileTransfer);
+    //let proxyurl = "https://cors-anywhere.herokuapp.com/";
     this.fileTransfer
       .download(
-        "https://devdactic.com/html/5-simple-hacks-LBT.pdf",
-        path + "myfile.pdf"
+        "https://www.adobe.com/support/products/enterprise/knowledgecenter/media/c4611_sample_explain.pdf",
+        path + "myFile.pdf"
       )
-      .then(
-        result => {
-          this.fileOpener.open(result.toURL(), "application/pdf");
-        },
-        error => {
-          this.presentAlert(error);
-        }
-      );
+      .then(entry => {
+        console.log("download complete: " + entry.toURL());
+        this.fileOpener
+          .open(entry.toURL(), "application/pdf")
+          .then(() => console.log("File is opened"))
+          .catch(e => console.log("Error opening file", e));
+      });*/
+    let url = "https://devdactic.com/html/5-simple-hacks-LBT.pdf";
+    FileSaver.saveAs(
+      "https://cors-anywhere.herokuapp.com/" + url,
+      "myFile.pdf"
+    );
+
+    FileSaver.saveAs(url, "myFile.pdf");
   }
   async presentAlert(error) {
     const alert = await this.alertController.create({
       header: " ",
       message: error,
-      buttons: [{
-        text: 'Cancel',
-        role: 'cancel',
-        cssClass: 'secondary',
-
-      }]
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          cssClass: "secondary"
+        }
+      ]
     });
 
     await alert.present();
